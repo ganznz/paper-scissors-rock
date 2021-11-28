3569346fb9b29e3cb952217885d634eda9888f7a
Ganz <loganbellinghamlb@gmail.com>
2021-11-28 14:31:45 +1300

    Added basic animations

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@ const startScreenDiv = document.querySelector(".start-screen");
 const startPlayButton = document.querySelector(".play-button");
 
 startPlayButton.addEventListener("click", () => {
-    startScreenDiv.remove();
+    startScreenDiv.classList.add("screen-fade")
+
+    // fires when .screen-fade transition ends because it's faster
+    // than the .play-button transition
+    startScreenDiv.addEventListener("transitionend", () => {
+        startScreenDiv.remove();    
+    })
 });
