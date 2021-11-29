a0b183780a64f13617f67bb65ca2b6a35d6823aa
Ganz <loganbellinghamlb@gmail.com>
2021-11-29 17:41:29 +1300

    Game completed

--- js/app.js
+++ js/app.js
@@ -15,8 +15,6 @@ let initialComputerScore = 0;
 startPlayButton.addEventListener("click", () => {
     startScreenDiv.classList.add("screen-fade")
 
-    // fires when .screen-fade transition ends because it's faster
-    // than the .play-button transition
     startScreenDiv.addEventListener("transitionend", () => {
         startScreenDiv.remove();
     })
@@ -43,16 +41,10 @@ const getComputerSelection = () => {
     return computerChoice;
 }
 
-function addEventListenerList(list, event, fn) {
-    for (var i = 0, len = list.length; i < len; i++) {
-        list[i].addEventListener(event, fn);
-    }
-}
-
 const playRound = (e) => {
     const computerSelection = getComputerSelection(); // Gets computers choice
 
-    // applies bg colour to selected choice
+    // apply bg colour to selected choice
     computerChoices.forEach(index => {
         if (index.classList.contains(computerSelection)) {
             index.classList.add("selected");
@@ -66,13 +58,11 @@ const playRound = (e) => {
     if (e.target.classList.contains("paper")) {
         switch (computerSelection) {
             case "scissors":
-                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`
-                console.log("you lost a point");
+                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`;
                 break;
 
             case "rock":
-                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`
-                console.log("you gained a point");
+                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`;
                 break;
         }
     }
@@ -80,13 +70,11 @@ const playRound = (e) => {
     if (e.target.classList.contains("scissors")) {
         switch (computerSelection) {
             case "rock":
-                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`
-                console.log("you lost a point");
+                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`;
                 break;
 
             case "paper":
-                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`
-                console.log("you gained a point");
+                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`;
                 break;
         }
     }
@@ -94,13 +82,11 @@ const playRound = (e) => {
     if (e.target.classList.contains("rock")) {
         switch (computerSelection) {
             case "paper":
-                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`
-                console.log("you lost a point");
+                computerScoreText.textContent = `SCORE: ${++initialComputerScore}`;
                 break;
 
             case "scissors":
-                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`
-                console.log("you gained a point");
+                playerScoreText.textContent = `SCORE: ${++initialPlayerScore}`;
                 break;
         }
     }
@@ -108,11 +94,18 @@ const playRound = (e) => {
 
 }
 
+// function that iterates an event over a nodelist
+function addEventListenerList(list, event, fn) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener(event, fn);
+    }
+}
+
 addEventListenerList(playerChoices, "click", (e) => {
-    // applies bg colour to selected choice
+    // apply bg colour to selected choice
     e.target.classList.add("selected");
 
-    // removes bg colour from other choices
+    // remove bg colour from other choices
     playerChoices.forEach(index => {
         if (!(index == e.target)) {
             index.classList.remove("selected")
@@ -120,5 +113,58 @@ addEventListenerList(playerChoices, "click", (e) => {
     });
 
     playRound(e);
-})
+    
+    if (initialPlayerScore >= 5 || initialComputerScore >= 5) {
+        if (initialPlayerScore > initialComputerScore) {
+          createEndScreen(`You won lesgooo! The score was ${initialPlayerScore} - ${initialComputerScore}`);  
+        } else {
+            createEndScreen(`You lost LLLL! The score was ${initialPlayerScore} - ${initialComputerScore}`);
+        }
+    }
+});
 
+const createEndScreen = (resultText) => {
+    const body = document.querySelector("body");
+    const gameContainer = document.querySelector(".game-container")
+
+    const div = document.createElement("div");
+    div.classList.add("end-screen");
+
+    const h1 = document.createElement("h1");
+    h1.classList.add("game-result-text");
+    h1.textContent = resultText;
+
+    const button = document.createElement("button");
+    button.classList.add("replay-button");
+    button.textContent = "PLAY AGAIN!";
+
+    div.appendChild(h1);
+    div.appendChild(button);
+    body.insertBefore(div, gameContainer);
+};
+
+const resetGame = () => {
+    initialPlayerScore = 0;
+    initialComputerScore = 0;
+    
+    playerChoices.forEach(index => { // resets player choices bg colours
+        index.classList.remove("selected")
+    });
+
+    computerChoices.forEach(index => { // resets player choices bg colours
+        index.classList.remove("selected")
+    });
+};
+
+document.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("replay-button")) {
+        const endScreenDiv = document.querySelector(".end-screen");
+        resetGame();
+
+        endScreenDiv.classList.add("screen-fade");
+        endScreenDiv.addEventListener("transitionend", () => {
+            endScreenDiv.remove();
+        })
+    }
+});
